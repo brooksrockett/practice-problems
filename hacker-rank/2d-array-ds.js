201c67141b6d8d83ec75b4bed8cb07ca3bbff4e1
Brooks Rockett <xylotism@gmail.com>
2020-03-26 00:18:54 -0700

    restructure for multiple challenge sites

---
+++ hacker-rank/2d-array-ds.js
@@ -0,0 +1,18 @@
+// Challenge URL:
+// https://www.hackerrank.com/challenges/2d-array/problem
+
+// Complete the hourglassSum function below.
+function hourglassSum(arr) {
+    var max = -Infinity; // started with 0 but doesn't account for all values being negative
+    var pos = [0,1,2,3]; // we only need to go up to index 3 in either direction
+    
+    for (let r of pos) {
+        for (let d of pos) {
+            var sum=arr[r][d]+arr[r][d+1]+arr[r][d+2]+arr[r+1][d+1]+arr[r+2][d]+arr[r+2][d+1]+arr[r+2][d+2]; // very sloppy, is there a cleaner way?
+            if (sum > max) {
+                max = sum;
+            }
+        }
+    }
+    return max;    
+}
